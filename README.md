4bd72871dc57f03760b479031f80d299f41a3b3f
Alex S <jalexspringer@gmail.com>
2018-06-05 17:38:57 -0400

    error handling, help docs

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Docker Image Update Frequency
 Command line tool to create visualizations and record update histories for docker hub images. Uses anchore navigator for update data. 
+
+See results.png for example output.
+
+## Installation
+git clone https://github.com/jalexspringer/image-updates.git
+cd image-updates
+pip install --user .
+
+## Usage
+image-update --help
+image-update -y 1 -o plot.png ubuntu centos:7 anchore/anchore-engine library/python:alpine
+
+## TODOS
+- Adjust figure height of output
+- Add help messages to click options and parameters
+
--- updates.py
+++ updates.py
@@ -25,21 +25,33 @@ def get_update_dictionary(repos, years):
     updates = {}
     for r in repos:
         ret = requests.get(req_url(r[0], r[1], r[2], get_id(r[0],r[1],r[2]))).json()
-        most_recent = datetime.fromtimestamp(ret['changelog']['history'][0]['created_at'])
-        second_most = datetime.fromtimestamp(ret['changelog']['history'][1]['created_at'])
         tag = '{}/{}:{}'.format(r[0],r[1],r[2])
-        updates[tag] = [most_recent, second_most]
-        while updates[tag][-1].year > datetime.now().year - years:
-            ret = requests.get(req_url(r[0], r[1], r[2], ret['changelog']['history'][1]['image_id'])).json()
-            updates[tag].append(datetime.fromtimestamp(ret['changelog']['history'][1]['created_at']))
-        print(tag, 'history loaded')
+        try:
+            most_recent = datetime.fromtimestamp(ret['changelog']['history'][0]['created_at'])
+            second_most = datetime.fromtimestamp(ret['changelog']['history'][1]['created_at'])
+            print('Found', tag, '- loading update history.')
+            updates[tag] = [most_recent, second_most]
+            try:
+                while updates[tag][-1].year > datetime.now().year - years:
+                    ret = requests.get(req_url(r[0], r[1], r[2], ret['changelog']['history'][1]['image_id'])).json()
+                    updates[tag].append(datetime.fromtimestamp(ret['changelog']['history'][1]['created_at']))
+                print(tag, 'history loaded')
+            except KeyError as e:
+                print('Image history is not that long!')
+                continue
+        except KeyError as e:
+            print(tag, '- image not found. Check spelling of image and tags.')
+            continue
     return updates
 
 @click.command()
-@click.argument('repos', nargs=-1)#, help='Repos to consider. Accepts the following formats: library/ubuntu:latest, ubuntu:latest, ubuntu')
-@click.option('--years', '-y', default=1)#, help='Number of years (from this one) to look back.')
-@click.option('--output', '-o', default='results.png')
+@click.argument('repos', nargs=-1)
+@click.option('--years', '-y', default=1, help='Number of years (from this one) to look back.')
+@click.option('--output', '-o', default='results.png', help='Output file for the plot.')
 def plot_updates(repos, years, output):
+    """This script returns the update frequency of Docker Hub images and generates a plot with update dates.
+       Pass any number of of images/repos - accepts the following formats: library/ubuntu:latest, ubuntu:latest, ubuntu'
+    """
     matplotlib.style.use('ggplot')
     reps = []
     fig, ax = plt.subplots(1)
